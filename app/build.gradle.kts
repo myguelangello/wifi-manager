368adb835a6b71547377af5ac0e09bb0682fea55
Myguel Angello <myguelangello624@gmail.com>
2026-03-03 11:32:50 -0300

    Update configs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,11 +22,11 @@ android {
         kotlinCompilerExtensionVersion = "1.5.8"
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -40,4 +40,5 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.android.material:material:1.12.0")
 }
